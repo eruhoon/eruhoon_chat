1f4ceecd70f778e84e514ffc9a6fd52ab99a7425
Marionette <belinecrecia@gmail.com>
2014-08-13 06:46:48 +0900

    -modify chatting
    내부 리팩토링

---
+++ js/chat.js
@@ -0,0 +1,57 @@
+var socket = io.connect('http://210.118.74.154:8124');
+
+socket.on('connect', function(){
+	var username = "";
+	while(username=="" || username==null){
+		username = prompt('Who are you?');
+	}
+	socket.emit('addme', username);
+});
+
+socket.on('chat', function(username, data){
+	var p = document.createElement('p');
+	p.innerHTML = username + ': ' + data;
+	document.getElementById('chatContainer').appendChild(p);
+});
+
+socket.on('afterSendChat', function(){
+    scrollDown();
+})
+
+window.addEventListener('load', function(){
+	resize();
+	document.getElementById('chatData').addEventListener('keypress', function(event){
+		enter_press(event);
+	});
+}, false);
+
+window.addEventListener('resize', function(){
+	resize();
+}, false)
+
+
+
+var sendChat = function(){
+    var text = document.getElementById('chatData').value;
+	document.getElementById('chatData').value = "";
+	socket.emit('onSendChat', text);
+}
+        
+var scrollDown = function(){
+	var chatContainer = document.getElementById('chatContainer');
+	chatContainer.scrollTop = chatContainer.scrollHeight;
+}
+
+var enter_press = function(event){
+	if(event.keyCode == 13){
+		sendChat();
+	}
+}
+
+var resize = function(){
+	var viewHeight = window.innerHeight;
+	var inputboxHeight = document.getElementsByClassName('chatting_inputbox')[0].clientHeight;
+
+	var chatContainer = document.getElementById('chatContainer');
+	chatContainer.style.height = (viewHeight - inputboxHeight) + "px";
+}
--- server.js
+++ server.js
@@ -1,38 +1,52 @@
-
 var app = require('http').createServer(handler);
 var io = require('socket.io').listen(app);
 var fs = require('fs');
 
-
 app.listen(8124);
 
+console.log("Server is created @ 8124 Port");
 
 function handler (req, res) {
-	fs.readFile(__dirname + '/index.html', function (err, data){
+	var requestURL = req.url;
+	var requestURLCheck = /^\/*$/;
+	
+	if(requestURLCheck.test(requestURL) == true){
+		requestURL = "/index.html";
+	}
+	
+	fs.readFile(__dirname + requestURL, function (err, data){
 		if(err){
 			res.writeHead(500);
-			return res.end('Error loading index.html');
+			return res.end('Loading Error');
 		}
-		counter = 1;
 		res.writeHead(200);
 		res.end(data);
 	});
 
 }
 
+
+
 io.sockets.on('connection', function (socket) {
-    
+
 	socket.on('addme', function(username){
+		if(username=="" || username==null){
+			return;
+		}
+		username = username.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
 		socket.username = username;
 		socket.emit('chat', 'SERVER', 'You Have connected');
 		socket.broadcast.emit('chat', 'SERVER', username+' is on deck');
+		io.sockets.emit('afterSendChat');
+		console.log("LOGIN : "+username+"("+socket.handshake.address.address+")");
 	});
 
+
 	socket.on('onSendChat', function(data){
-        if(data.trim()==""){
+        if(data.trim()=="" || socket.username==null){
             return;
         }
-        //data = encodeURI(data);
+        data = data.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
 		io.sockets.emit('chat', socket.username, data);
         io.sockets.emit('afterSendChat');
 	});
@@ -40,6 +54,7 @@ io.sockets.on('connection', function (socket) {
 
 	socket.on('disconnect', function(){
 		io.sockets.emit('chat', 'SERVER', socket.username + ' has left the building');
+		console.log("LOGOUT : "+socket.username);
 	});
 
 });
